aeb229550ff59d2b384ebffc8b10de7bc5f57bd9
Negr087 <spibanezpaypal@gmail.com>
2026-03-18 16:02:45 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: false,
-  },
-  // Agregar estas configuraciones:
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
   },
 };
 
